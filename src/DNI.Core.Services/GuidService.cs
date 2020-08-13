8dc7b6d26c388b889181c21d7351036a4164e505
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-08-13 19:45:36 +0100

    Implemented GuidService

---
+++ src/DNI.Core.Services/GuidService.cs
@@ -0,0 +1,17 @@
+﻿using DNI.Core.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Services
+{
+    public class GuidService : IGuidService
+    {
+        public Guid GenerateGuid()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
