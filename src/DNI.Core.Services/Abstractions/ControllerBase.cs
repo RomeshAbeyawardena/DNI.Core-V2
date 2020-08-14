f15dccc4366f0d508d72cc2c20b4f6b1d81498d8
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-08-14 12:20:43 +0100

    Added controller base

---
+++ src/DNI.Core.Services/Abstractions/ControllerBase.cs
@@ -0,0 +1,44 @@
+﻿using DNI.Core.Contracts.Providers;
+using DNI.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNI.Core.Services.Abstractions
+{
+    public abstract class ExtendedControllerBase : ControllerBase
+    {
+        protected ExtendedControllerBase(IMapperProvider mapperProvider)
+        {
+            Mapper = mapperProvider;
+        }
+
+        protected IMapperProvider Mapper { get; }
+
+        protected IActionResult ValidateResponse<T> (
+            IResponse<T> response, 
+            Func<T, IActionResult> runOnSuccessful = null,
+            Func<T, Exception, IActionResult> runOnFailure = null)
+        {
+            if(runOnSuccessful == null)
+            {
+                runOnSuccessful = result => Ok(result);
+            }
+
+            if(runOnFailure == null)
+            {
+                runOnFailure = (result, exception) => BadRequest(exception);
+            }
+
+            if (response.IsSuccessful)
+            {
+                return runOnSuccessful(response.Result);
+            }
+
+            return runOnFailure(response.Result, response.Exception);
+        }
+    }
+}
